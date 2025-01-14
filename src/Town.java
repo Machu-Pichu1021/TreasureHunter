a0b403a74409f4c239ad75d0e4b81d4703c6c63c
BT_1N3_12 <sample@bths.edu>
2025-01-14 13:15:39 -0500

    Completed TH4 (marsh)

--- src/Town.java
+++ src/Town.java
@@ -70,8 +70,12 @@ public class Town {
             }
             return true;
         }
-
-        printMessage = "You can't leave town, " + hunter.getHunterName() + ". You don't have a " + terrain.getNeededItem() + ".";
+        if (terrain.getNeededItem().equals("boots")) {
+            printMessage = "You can't leave town, " + hunter.getHunterName() + ". You don't have " + terrain.getNeededItem() + ".";
+        }
+        else{
+            printMessage = "You can't leave town, " + hunter.getHunterName() + ". You don't have a " + terrain.getNeededItem() + ".";
+        }
         return false;
     }
 
@@ -124,16 +128,18 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < 1.0/6) {
             return new Terrain("Mountains", "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < 2.0/6) {
             return new Terrain("Ocean", "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < 3.0/6) {
             return new Terrain("Plains", "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < 4.0/6) {
+            return new Terrain("Desert", "Water");
+        } else if (rnd < 5.0/6) {
             return new Terrain("Desert", "Water");
         } else {
-            return new Terrain("Jungle", "Machete");
+            return new Terrain("Marsh", "Boots");
         }
     }
 
